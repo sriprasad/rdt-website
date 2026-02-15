59748d719e4eb0dda6877c34df661ededf7e9d75
sriprasad <sriprasad.06@gmail.com>
2026-02-15 17:14:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: isProd ? "/rdt-website" : "",
+  assetPrefix: isProd ? "/rdt-website/" : "",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
