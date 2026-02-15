966f1a39ca00cd33a94642d9b31f88d2777dc370
sriprasad <sriprasad.06@gmail.com>
2026-02-15 18:13:30 +0530

    Updated homepage layout

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
+const repoName = "rdt-website";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/rdt-website" : "",
-  assetPrefix: isProd ? "/rdt-website/" : "",
-  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath:isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "" ,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
